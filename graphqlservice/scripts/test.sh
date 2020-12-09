60eefb05ef883234e7ae5350cf2f32ce18840af3
hpratt <hpratt111@live.com>
2020-12-09 16:14:05 -0500

    make sure database is up before testing

--- graphqlservice/scripts/test.sh
+++ graphqlservice/scripts/test.sh
@@ -7,5 +7,5 @@ source ./scripts/setup-environment.sh
 
 scripts/run-dependencies.sh
 yarn install
-yarn test
+docker exec graphqlservice_postgrestest_1 psql -c "select 1" --user postgres > /dev/null 2>&1 && yarn test
 scripts/stop-dependencies.sh
