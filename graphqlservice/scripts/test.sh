126e44658dae62cfdb7df2f374a961bfbbd9d1c1
hpratt <hpratt111@live.com>
2020-12-09 15:23:26 -0500

    try reading environment variables

--- graphqlservice/scripts/test.sh
+++ graphqlservice/scripts/test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
+# cd to project root directory
 cd "$(dirname "$(dirname "$0")")"
+source ./scripts/setup-environment.sh
+
 scripts/run-dependencies.sh
 yarn install
 yarn test
